ea33f02eb28d7849135fa1c6f8eabf6c132e423b
Hasan Aysal <103414704+hasannaysall@users.noreply.github.com>
2025-01-04 21:19:59 +0300

    Update transaction_4.sql

--- HastaKayitOtomasyonu/Transaction/transaction_4.sql
+++ HastaKayitOtomasyonu/Transaction/transaction_4.sql
@@ -1,23 +1,23 @@
--- 4. Birden Fazla Tabloyu Güncelleme
+
 BEGIN TRANSACTION;
 
 BEGIN TRY
-    -- Hasta bilgisi güncelleme
+  
     UPDATE Hasta
     SET Ad = 'Ahmet', Soyad = 'Demir'
     WHERE HastaID = 2;
 
-    -- Refakatçi bilgisi güncelleme
+ 
     UPDATE Refakatçi
     SET Ad = 'Zeynep', Soyad = 'Kaya'
     WHERE RefakatçiID = 5;
 
-    -- İşlemi tamamla
+  
     COMMIT TRANSACTION;
     PRINT 'Güncellemeler başarıyla tamamlandı.';
 END TRY
 BEGIN CATCH
-    -- Hata durumunda işlemi geri al
+  
     ROLLBACK TRANSACTION;
     PRINT 'Hata oluştu. Tüm işlemler geri alındı.';
 END CATCH;
