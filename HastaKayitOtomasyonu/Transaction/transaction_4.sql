0552d60e175d4bacab2b5274e60101b71ef82d3a
Hasan Aysal <103414704+hasannaysall@users.noreply.github.com>
2025-01-06 23:28:29 +0300

    Update transaction_4.sql

--- HastaKayitOtomasyonu/Transaction/transaction_4.sql
+++ HastaKayitOtomasyonu/Transaction/transaction_4.sql
@@ -1,23 +1,33 @@
-
 BEGIN TRANSACTION;
 
 BEGIN TRY
-  
+    -- Hasta tablosunu güncelle
     UPDATE Hasta
     SET Ad = 'Ahmet', Soyad = 'Demir'
-    WHERE HastaID = 2;
+    WHERE HastaID = 100;
 
- 
+    -- Etkilenen satır sayısını kontrol et
+    IF @@ROWCOUNT = 0
+        THROW 51000, 'Hasta tablosunda belirtilen ID bulunamadı.', 1;
+
+    -- Refakatçi tablosunu güncelle
     UPDATE Refakatçi
     SET Ad = 'Zeynep', Soyad = 'Kaya'
-    WHERE RefakatçiID = 5;
+    WHERE RefakatçiID = 5555;
 
-  
+    -- Etkilenen satır sayısını kontrol et
+    IF @@ROWCOUNT = 0
+        THROW 51001, 'Refakatçi tablosunda belirtilen ID bulunamadı.', 1;
+
+    -- İşlemleri onayla
     COMMIT TRANSACTION;
     PRINT 'Güncellemeler başarıyla tamamlandı.';
 END TRY
 BEGIN CATCH
-  
+    -- İşlemleri geri al
     ROLLBACK TRANSACTION;
-    PRINT 'Hata oluştu. Tüm işlemler geri alındı.';
+
+    -- Hata mesajını göster
+    PRINT ERROR_MESSAGE();
 END CATCH;
+
